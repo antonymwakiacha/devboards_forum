292add7190e8aa21392c6b6ba0ef4b8946e27973
latifanoor <tonywugoma@gmail.com>
2019-08-21 21:09:13 +0300

    added login_url variable

--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -135,3 +135,4 @@ STATICFILES_DIRS=[
 LOGOUT_REDIRECT_URL='home'
 LOGIN_REDIREECT_URL='home'#tells django to redirect the user after a successful login.
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+LOGIN_URL='login'
