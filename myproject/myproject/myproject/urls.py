7e4afccb5faf65d8aa41dada06b272916609f55f
latifanoor <tonywugoma@gmail.com>
2019-08-23 19:01:37 +0300

    added new routes for topic posts

--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
    # re_path(r'^boards/(?P<pk>\d+)/$',views.board_topics,name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$',views.topic_posts,name='topic_posts'),
     #path('board/new',views.new_topic,name='new_topic'),
     #url(r'^boards/(?P<pk>\d+)/new/$',views.new_topic,name='new_topic'),
     #path('admin/', admin.site.urls),
