ae9165caf2cdadf1d6baf6ae1003e8bdc14e88fb
latifanoor <tonywugoma@gmail.com>
2019-10-03 12:47:53 +0300

    added humanize,gravatar functionalities and other minor modifications

--- myproject/myproject/boards/models.py
+++ myproject/myproject/boards/models.py
@@ -1,3 +1,4 @@
+import math
 from django.db import models
 from django.utils.text import Truncator
 # Create your models here.
@@ -28,6 +29,26 @@ class Topic(models.Model):
 
 	def __str__(self):
 		return self.subject
+
+	def get_page_count(self):
+		count=self.posts.count()
+		pages=count/20
+		return math.ceil(pages)
+
+	def has_many_pages(self,count=None):
+		if count is None:
+			count=self.get_page_count()
+		return count>6
+
+	def get_page_range(self):
+		count=self.get_page_count()
+		if self.has_many_pages(count):
+			return range(1,5)
+		return range(1,count +1)
+
+	def get_last_ten_posts(self):
+		return self.posts.order_by('-created_at')[:10]
+
 class Post(models.Model):
 	message=models.TextField(max_length=4000)
 	topic=models.ForeignKey(Topic,related_name='posts',on_delete=models.DO_NOTHING)
